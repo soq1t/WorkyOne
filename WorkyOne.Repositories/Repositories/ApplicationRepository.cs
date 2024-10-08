747af1f71d1049c3493800892f6d3ebb82cd6a7d
Andrei Gruzinov <andrei_gruzinov@list.ru>
2024-10-08 22:28:04 +0300

    Создал IBaseRepository

---
+++ WorkyOne.Repositories/Repositories/ApplicationRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorkyOne.AppServices.Interfaces.Repositories;
+using WorkyOne.Contracts.Repositories;
+using WorkyOne.Domain.Abstractions;
+using WorkyOne.Domain.Entities.Schedule;
+using WorkyOne.Domain.Interfaces;
+using WorkyOne.Repositories.Contextes;
+
+namespace WorkyOne.Repositories.Repositories
+{
+    /// <summary>
+    /// Базовый репозиторий приложения
+    /// </summary>
+    public class ApplicationRepository : IBaseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<RepositoryResult> CreateAsync<TEntity>(TEntity entity)
+            where TEntity : EntityBase
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RepositoryResult> CreateManyAsync<TEntity>(IEnumerable<TEntity> entities)
+            where TEntity : EntityBase
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RepositoryResult> DeleteAsync<TEntity>(string entityId)
+            where TEntity : EntityBase
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RepositoryResult> DeleteManyAsync<TEntity>(IEnumerable<string> entitiesIds)
+            where TEntity : EntityBase
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity?> GetAsync<TEntity>(IQueryable<TEntity> query)
+            where TEntity : EntityBase
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetManyAsync<TEntity>(IQueryable<TEntity> query)
+            where TEntity : EntityBase
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
