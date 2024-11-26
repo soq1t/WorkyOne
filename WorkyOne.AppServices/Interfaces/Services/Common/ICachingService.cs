d3cd728c8866cd75a0235174a39ce1d93de7b8f4
soq1t <andrei_gruzinov@list.ru>
2024-11-26 15:25:16 +0300

    Создал ICachingService

---
+++ WorkyOne.AppServices/Interfaces/Services/Common/ICachingService.cs
@@ -0,0 +1,39 @@
+﻿namespace WorkyOne.AppServices.Interfaces.Services.Common
+{
+    /// <summary>
+    /// Интерфейс сервиса, работающего с кешем
+    /// </summary>
+    public interface ICachingService
+    {
+        /// <summary>
+        /// Возвращает значение из кеша. При остутствии значения, выполняет <paramref name="function"/>, возвращает результат выполнения и сохраняет его в кеш
+        /// </summary>
+        /// <typeparam name="T">Тип возвращаемого объекта</typeparam>
+        /// <param name="key">Ключ для поиска в кеше</param>
+        /// <param name="duration">Период, на который сохраняется результат выполнения <paramref name="function"/></param>
+        /// <param name="function">Выполняемая функция по получению данных</param>
+        /// <param name="cancellation">Токен отмены задачи</param>
+        public Task<T?> GetAsync<T>(
+            string key,
+            Func<T>? function = null,
+            TimeSpan? duration = null,
+            CancellationToken cancellation = default
+        )
+            where T : class;
+
+        /// <summary>
+        /// Сохраняет объект в кеше
+        /// </summary>
+        /// <typeparam name="T">Тип сохраняемого объекта</typeparam>
+        /// <param name="key">Ключ для сохранения в кеше</param>
+        /// <param name="value">Сохраняемый объект</param>
+        /// <param name="duration">Период, на который сохраняется объект</param>
+        /// <param name="cancellation">Токен отмены задачи</param>
+        public Task SaveAsync<T>(
+            string key,
+            T value,
+            TimeSpan duration,
+            CancellationToken cancellation = default
+        );
+    }
+}
---
+++ WorkyOne.Infrastructure/Services/CachingService.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using WorkyOne.AppServices.Interfaces.Services.Common;
+
+namespace WorkyOne.Infrastructure.Services
+{
+    /// <summary>
+    /// Сервис, работающий с кешем
+    /// </summary>
+    public class CachingService : ICachingService
+    {
+        private readonly IDistributedCache _cache;
+
+        public CachingService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<T?> GetAsync<T>(
+            string key,
+            Func<T>? function = null,
+            TimeSpan duration = default,
+            CancellationToken cancellation = default
+        )
+            where T : class
+        {
+            var prefix = typeof(T).Name;
+
+            string prefixedKey = prefix + "_" + key;
+
+            var json = await _cache.GetStringAsync(prefixedKey, cancellation);
+
+            if (json == null)
+            {
+                if (function == null)
+                {
+                    return null;
+                }
+
+                var result = function.Invoke();
+                await SaveAsync(key, result, duration, cancellation);
+
+                return result;
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+        }
+
+        public Task<T?> GetAsync<T>(
+            string key,
+            Func<T>? function = null,
+            TimeSpan? duration = null,
+            CancellationToken cancellation = default
+        )
+            where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveAsync<T>(
+            string key,
+            T value,
+            TimeSpan duration,
+            CancellationToken cancellation = default
+        )
+        {
+            var prefix = typeof(T).Name;
+            var prefixedKey = prefix + "_" + key;
+            var json = JsonConvert.SerializeObject(value);
+
+            await _cache.SetStringAsync(
+                prefixedKey,
+                json,
+                new DistributedCacheEntryOptions() { AbsoluteExpirationRelativeToNow = duration },
+                cancellation
+            );
+        }
+    }
+}
