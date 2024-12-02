3f2109f9015346603c4ac28b9227c88ecb996cf9
soq1t <andrei_gruzinov@list.ru>
2024-12-02 15:37:24 +0300

    Добавил Docker support

--- WorkyOne.MVC/Middlewares/AccessFiltersMiddleware.cs
+++ WorkyOne.MVC/Middlewares/AccessFiltersMiddleware.cs
@@ -18,17 +18,10 @@ namespace WorkyOne.MVC.Middlewares
             ILogger<AccessFiltersMiddleware> logger
         )
         {
-            var id = Guid.NewGuid().ToString();
-            logger.LogInformation("{1}: Запрос фильтра доступа", id);
-
             var timer = new Stopwatch();
             timer.Start();
             await filtersStore.CreateFiltersAsync();
-            logger.LogInformation(
-                "{1}: Фильтр доступа получен ({2} ms)",
-                id,
-                timer.ElapsedMilliseconds
-            );
+            logger.LogDebug("Фильтр доступа получен ({1} ms)", timer.ElapsedMilliseconds);
             timer.Stop();
 
             await _next(context);
